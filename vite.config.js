6a1e628d98dea43e96b6eb8450be1157ffacc592
Lucialva1 <alvarezaluciano@gmail.com>
2026-03-06 10:34:37 -0500

    config change for hosting change

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from'@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Student-Portal-Frontend/',
+  base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'https://the-ah-api-hkgfbafrczcyf5bb.eastus2-01.azurewebsites.net',
+        target: 'https://ahfunctionapp-dehcd9ehe6h6c5fc.eastus2-01.azurewebsites.net/api',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
